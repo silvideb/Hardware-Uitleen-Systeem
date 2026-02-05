022404b1cd1753f4b180fefb9080199c305bc55c
silvio de boer <191617@novacollege.nl>
2026-02-05 15:19:24 +0100

    status

--- database/migrations/2026_02_05_132144_create_loans_table.php
+++ database/migrations/2026_02_05_132144_create_loans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('status');
             $table->date('start_date');
             $table->date('due_date');
             $table->timestamps();
